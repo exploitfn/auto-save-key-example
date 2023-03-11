80fce23f235abfbcad3016cb3da4659f74b94abc
exploitfn <121313674+exploitfn@users.noreply.github.com>
2023-03-11 10:46:11 -0330

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+
+auto main() -> int
+{
+     std::string key;
+     std::cout << ("Key: ");
+     std::cin >> key;
+     if (key == "yourkey")
+     {
+          ofstream file;
+          file.open("filepath.txt");
+          file << key;
+     }
+}
